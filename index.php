c3d68d92a5108b28a0e1ed9ac7cf2e5220c97ba7
Hanna Sarokina <tore.smile.01@gmail.com>
2020-05-27 17:44:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Brrraddy</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css"
+</head>
+
+<body>
+    <header>
+        <div id="logo">
+            <a href="/" title="Back to first page"><span>Brrr</span>addy</a>
+        </div>
+
+        <div id="menuHead">
+            <a href="/about.php">
+            <div style="margin-right: 5%">About us</div>
+            </a>
+            <a href="/feedback.php"><div>Feedback</div></a>
+        </div>
+
+        <div id="regAuth">
+            <a href="/reg.php">
+                <div>Registration</div>
+            </a>
+            <a href="/auth.php"<div>Authorisation</div></a>
+        </div>
+    </header>
+</body>
+
+    <div id="wrapper">
+        <div id="leftCol">
+            <div id="bigArticle">
+                <img src="img/article_1.jpg" alt="First article" title="First article">
+                <h2>First article</h2>
+                <p>
+                    I say he look'd on, if I must be content
+                    To stay him from the fatal of our country's bliss.
+                    His lordship pluck'd from this sentence then for prey,
+                    And then let us twain, being the moon,
+                    were she such a case as fills m
+                </p>
+                <div>Next</div>
+            </div>
+            <div class="clear">
+
+            </div>
+
+            <div class="article">
+
+            </div>
+        </div>
+
+        <div id="rightCol">
+            <div class="banner">
+                <img src="img/banner_1.jpg" alt="Banner 1" title="Banner 1">
+            </div>
+            <div class="banner">
+                <img src="img/banner_2.jpg" alt="Banner 2" title="Banner 2">
+            </div>
+        </div>
+    </div>
+
+<footer>
+    <div id="social">
+        <a href="http://vk.com/brrraddy" title="Hanna Sarokina" target="_blank">
+        <img src="img/vk.png" alt="vk" title="Vk">
+        </a>
+        <a href="http://facebook.com/brrraddy" title="Hanna Sarokina" target="_blank">
+        <img src="img/facebook.png" alt="facebook" title="Facebook">
+        </a>
+        <a href="http://instagram.com/brrraddy" title="Hanna Sarokina" target="_blank">
+        <img src="img/inst.png" alt="inst" title="Hanna Sarokina">
+        </a>
+    </div>
+    <div id="rights">
+        All rights reserved &copy; <?php echo date('Y')?>
+    </div>
+</footer>
+</html>
